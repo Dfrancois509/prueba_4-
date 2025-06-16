31b8acb430ed03ae55fac32478123061b03b9d28
Dfrancois509 <de.francois@duocuc.cl>
2025-06-16 11:44:04 -0400

    Add files via upload

---
+++ #Zapatillas Strike.py
@@ -0,0 +1,41 @@
+#Zapatillas Strike
+elecciona_opcion = 0
+def main(): 
+    global elecciona_opcion
+
+    print('Menu')
+    print('Opcion 1 ) Reservar Zapatillas')
+    print('Opcion 2 ) Buscar Zapatillas Reservadas')
+    print('Opcion 3 ) Cancelar reserva de Zapatillas')
+    print('Opcion 4 ) Salir')
+    elecciona_opcion = int(input('Seleccione una opcion: '))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def ejecuta():
+    while True: 
+        main() 
